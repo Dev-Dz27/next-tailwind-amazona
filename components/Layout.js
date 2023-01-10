d2fd5d0bc30e0616e77f5be1d7a7f5a552e9c868
Chekkal Youcef <81917154+Dev-Dz27@users.noreply.github.com>
2023-01-10 19:38:07 +0100

    update the footer year
    display the current year on the Copyright © < YEAR > Amazona.
    without the need to update it manually each year

--- components/Layout.js
+++ components/Layout.js
@@ -133,7 +133,7 @@ export default function Layout({ title, children }) {
         </header>
         <main className="container m-auto mt-4 px-4">{children}</main>
         <footer className="flex h-10 justify-center items-center shadow-inner">
-          <p>Copyright © 2022 Amazona</p>
+          <p>Copyright © {(new Date().getFullYear())} Amazona</p>
         </footer>
       </div>
     </>
